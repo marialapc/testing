f8f944c7d550812bea6824f3e92686994eb05a19
marialapc <marialopez.apc@gmail.com>
2023-04-20 13:15:11 +0200

    try groups and beforeAll

---
+++ demos/src/05-setup.test.js
@@ -0,0 +1,20 @@
+describe('Set', () => {
+    beforeAll(() => {
+        console.log('beforeAll');
+    });
+ test('case 1', () => {
+   expect(1 + 1).toBe(2);
+ });
+ test('case 2', () => {
+    expect(1 + 3).toBe(4);
+  }); 
+});
+
+describe('other group', () => {
+    test('case 3', () => {
+      expect(1 + 1).toBe(2);
+    });
+    test('case 4', () => {
+       expect(1 + 3).toBe(4);
+     }); 
+   });
