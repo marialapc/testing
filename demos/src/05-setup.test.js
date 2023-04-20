531e1876f291d2ce0eca3614eea2873be63614bd
marialapc <marialopez.apc@gmail.com>
2023-04-20 16:54:46 +0200

    test 6

--- demos/src/05-setup.test.js
+++ demos/src/05-setup.test.js
@@ -2,19 +2,31 @@ describe('Set', () => {
     beforeAll(() => {
         console.log('beforeAll');
     });
+    afterAll(() => {
+        console.log('afterAll');
+    });
+
+    beforeEach(() => {
+    console.log('beforeEach');
+    });
+
  test('case 1', () => {
+    console.log('case 1');
    expect(1 + 1).toBe(2);
  });
  test('case 2', () => {
+    console.log('case 2');
     expect(1 + 3).toBe(4);
   }); 
 });
 
 describe('other group', () => {
     test('case 3', () => {
+        console.log('case 3');
       expect(1 + 1).toBe(2);
     });
     test('case 4', () => {
+        console.log('case 4');
        expect(1 + 3).toBe(4);
      }); 
    });
