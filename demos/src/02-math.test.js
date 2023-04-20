51e05425387851b5f53394b7f791f81f7b89b372
marialapc <marialopez.apc@gmail.com>
2023-04-20 12:39:37 +0200

    update package.json

--- demos/src/02-math.test.js
+++ demos/src/02-math.test.js
@@ -1,18 +1,18 @@
 const {sum, multiply, divide} = require('./02-math');
 
-test("add 1 + 3 should be 4", () => {
-    const respuesta = sum (1,3);
+test('add 1 + 3 should be 4', () => {
+    const respuesta = sum (1, 3);
     expect(respuesta).toBe(4);
 });
 
-test("should be 4", () => {
-    const respuesta = multiply(1,4);
+test('should be 4', () => {
+    const respuesta = multiply(1, 4);
     expect(respuesta).toBe(4);
 });
 
-test("should divide", () => {
-    const respuesta = divide(6,3);
+test('should divide', () => {
+    const respuesta = divide(6, 3);
     expect(respuesta).toBe(2);
-    const respuesta2 = divide(5,2);
+    const respuesta2 = divide(5, 2);
     expect(respuesta2).toBe(2.5);
 });
